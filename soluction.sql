edec679165fb063dff5ccd3015475dfa65777357
Axhu99 <xhuliodreni@icloud.com>
2024-03-08 18:12:48 +0100

    first soluction

---
+++ soluction.sql
@@ -0,0 +1,35 @@
+-- 1. Selezionare tutti gli studenti nati nel 1990 (160)
+SELECT * FROM `students` WHERE YEAR (`date_of_birth`) = 1990;
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT * FROM `courses` WHERE `cfu` > 10;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * FROM `students` WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * FROM `courses` WHERE `year` = 1 AND `period` = 'I Semestre';
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` >= '14:%';
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*) AS `num_dipartimento` FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(`phone`) AS `senza_telefono` FROM `teachers`;
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`enrolment_date`) AS `anno_iscrizione`, COUNT(*) AS `numero_iscritti` FROM `students` GROUP BY `anno_iscrizione`;
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address` AS `indirizzo_ufficio`, COUNT(*) AS `numero_insegnanti` FROM `teachers` GROUP BY `office_address` HAVING `numero_insegnanti` > 1;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`)) AS `media_voto`, `exam_id` FROM `exam_student` GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(*) AS `numero_corsi`, `department_id` AS `id_dipartimento` FROM `degrees` GROUP BY `department_id`;
